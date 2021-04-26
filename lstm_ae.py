7525b4822f613efc5730f57f3864f77854952d86
Eyal <eyalseg@post.bgu.ac.il>
2021-04-26 20:08:03 +0300

    implemented an LSTM auto-encoder + basic tests

---
+++ lstm_ae.py
@@ -0,0 +1,86 @@
+import numpy as np
+import torch as T
+import torch.nn as nn
+
+DEVICE = T.device('cuda' if T.cuda.is_available() else 'cpu')
+
+
+def init_weights(lstm:nn.LSTM):
+    # for name, param in lstm.named_parameters():
+    #     if 'bias' in name:
+    #         nn.init.constant(param, 0.0)
+    #     elif 'weight' in name:
+    #         nn.init.xavier_normal(param)
+    nn.init.xavier_uniform(lstm.weight_ih_l0, gain=np.sqrt(2))
+    nn.init.xavier_uniform(lstm.weight_hh_l0, gain=np.sqrt(2))
+
+
+class LstmAutoEncoder(nn.Module):
+    def __init__(self, input_dim, encoded_dim, num_layers=1, device=DEVICE):
+        super(LstmAutoEncoder, self).__init__()
+
+        self.input_dim = input_dim
+        self.encoded_dim = encoded_dim
+        self.num_layers = num_layers
+        self.device = None
+
+        self.encoder = nn.LSTM(input_size=input_dim,
+                               hidden_size=encoded_dim,
+                               num_layers=num_layers,
+                               batch_first=True)
+        init_weights(self.encoder)
+
+        self.decoder = nn.LSTM(input_size=encoded_dim,
+                               hidden_size=input_dim,
+                               num_layers=num_layers,
+                               batch_first=True)
+        init_weights(self.decoder)
+
+        self.encoder_activation = nn.ReLU()
+        self.decoder_activation = nn.Sigmoid()
+
+        self.to(device)
+
+    def _encode(self, X):
+        h0 = T.randn(self.num_layers, X.size(0), self.encoded_dim)
+        c0 = T.randn(self.num_layers, X.size(0), self.encoded_dim)
+
+        h0 = T.autograd.Variable(h0).to(self.device)
+        c0 = T.autograd.Variable(c0).to(self.device)
+
+        X, (h_n, c_n) = self.encoder(X, (h0, c0))
+        # X, (h_n, c_n) = self.encoder(X)
+        # todo: check if an activation method is needed
+        h_n = h_n[-1]
+        h_n = self.encoder_activation(h_n)
+
+        return h_n
+
+    def _decode(self, encoded_input):
+        h0 = T.randn(self.num_layers, encoded_input.size(0), self.input_dim)
+        c0 = T.randn(self.num_layers, encoded_input.size(0), self.input_dim)
+
+        h0 = T.autograd.Variable(h0).to(self.device)
+        c0 = T.autograd.Variable(c0).to(self.device)
+
+        decoded, hidden_state = self.decoder(encoded_input, (h0, c0))
+        # decoded, hidden_state = self.decoder(encoded_input)
+        # todo: check if an activation method is needed
+        # decoded = self.decoder_activation(decoded)
+        return self.decoder_activation(decoded)
+
+    def forward(self, X):
+        z = self._encode(X)
+        z = z.unsqueeze(1).repeat(1, X.shape[1], 1)
+        x_ = self._decode(z)
+
+        return x_
+
+    def to(self, device):
+        self.device = device
+
+        self.encoder = self.encoder.to(device)
+        self.decoder = self.decoder.to(device)
+
+        return super(LstmAutoEncoder, self).to(device)
+
---
+++ tests/test_lstm_ae.py
@@ -0,0 +1,77 @@
+import pytest
+
+import numpy as np
+import torch as T
+
+from torch import nn, optim
+from sklearn.linear_model import LinearRegression
+
+from lstm_ae import LstmAutoEncoder
+
+
+device = 'cuda:0' if T.cuda.is_available() else 'cpu'
+
+
+class Test_Lstm_AE:
+
+    @pytest.fixture()
+    def ae(self, seq_dim, latent_size, num_layers):
+        return LstmAutoEncoder(seq_dim, latent_size, num_layers).to(device)
+
+    @pytest.mark.parametrize("seq_length", [1, 50])
+    @pytest.mark.parametrize("latent_size", [1, 20])
+    @pytest.mark.parametrize("batch_size", [1, 100])
+    @pytest.mark.parametrize("seq_dim", [1, 5])
+    @pytest.mark.parametrize("num_layers", [1, 2])
+    def test_encoded_shape(self, seq_length, latent_size, batch_size, seq_dim, num_layers, ae):
+        input = T.randn(batch_size, seq_length, seq_dim).to(device)
+
+        encoded = ae._encode(input)
+        assert encoded.shape == (batch_size, latent_size)
+
+    @pytest.mark.parametrize("seq_length", [50])
+    @pytest.mark.parametrize("latent_size", [20])
+    @pytest.mark.parametrize("batch_size", [1, 100])
+    @pytest.mark.parametrize("seq_dim", [1, 5])
+    @pytest.mark.parametrize("num_layers", [1, 2])
+    def test_output_shape(self, seq_length, latent_size, batch_size, seq_dim, num_layers, ae):
+        input = T.randn(batch_size, seq_length, seq_dim).to(device)
+
+        with T.no_grad():
+            output = ae.forward(input)
+
+        assert input.shape == output.shape, "Output is not at the same shape of the input!"
+
+    @pytest.mark.parametrize("seq_length", [50])
+    @pytest.mark.parametrize("latent_size", [20])
+    @pytest.mark.parametrize("batch_size", [1, 100])
+    @pytest.mark.parametrize("seq_dim", [1, 5])
+    @pytest.mark.parametrize("num_layers", [1, 2])
+    @pytest.mark.parametrize("input", [
+        lambda batch, length, dim: T.randn(batch, length, dim, dtype=T.float32).to(device),
+    ])
+    def test_fit(self, seq_length, latent_size, batch_size, seq_dim, num_layers, input, ae):
+        # x = np.linspace(-np.pi, np.pi, seq_length)
+        # input = T.tensor([[np.sin(x)]], dtype=T.float32).to(device)
+
+        input = input(batch_size, seq_length, seq_dim)
+
+        criterion = nn.MSELoss()
+        optimizer = optim.SGD(ae.parameters(), lr=0.2)
+
+        losses = []
+        for epoch in range(500):
+            optimizer.zero_grad()
+
+            output = ae.forward(input)
+            loss = criterion(output, input)
+            loss.backward()
+            optimizer.step()
+
+            losses.append(loss.item())
+
+        x = [i for i in range(len(losses))]
+        coeffs = np.polyfit(x, losses, 1)
+        slope = coeffs[0]
+
+        assert slope < -0, "Learning does not decrease the training loss!"
