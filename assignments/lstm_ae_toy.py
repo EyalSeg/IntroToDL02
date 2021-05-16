1e8918d60c54eea9e0209d0b0a07d2d54d01913b
Nitsan Soffair <nitsan.soffair@gmail.com>
2021-05-16 17:14:01 +0300

    Eyal comments

--- assignments/lstm_ae_toy.py
+++ assignments/lstm_ae_toy.py
@@ -24,7 +24,7 @@ if __name__ == "__main__":
     mse = nn.MSELoss()
 
     supervised = False
-    regression = True
+    regression = False
 
     criterion = lambda output, input: mse(output.output_sequence, input)
 
